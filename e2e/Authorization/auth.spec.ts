b3f0105a3a3e9ae121a21f6b1750c2800ce4ba21
Ochilov <57859849+Ikromjon1998@users.noreply.github.com>
2023-04-20 09:49:54 +0200

    added tests

---
+++ e2e/Authorization/auth.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+// @ts-check
+test('Login Page', async ({ page }) => {
+    await page.goto('http://localhost/');
+    await page.getByRole('link', { name: 'Log in' }).click();
+
+    await expect(page).toHaveURL(/.*login/)
+});
+
+test('Dushboard', async ({ page }) => {
+    await page.goto('http://localhost/dashboard');
+
+    await page.getByLabel('Email').fill('test@example.com');
+    await page.getByLabel('Password').fill('password');
+    await page.getByLabel('Password').press('Enter');
+
+    await expect(page).toHaveURL(/.*dashboard/);
+
+    await expect(page.getByText('You\'re logged in!')).toHaveText('You\'re logged in!');
+
+});
+
+test('Logout', async ({ page }) => {
+    await page.goto('http://localhost/dashboard');
+
+    await page.getByLabel('Email').fill('test@example.com');
+    await page.getByLabel('Password').fill('password');
+    await page.getByLabel('Password').press('Enter');
+
+    await page.getByRole('link', { name: 'Log out' }).click();
+
+    await expect(page).toHaveURL(/localhost/);
+});
+
